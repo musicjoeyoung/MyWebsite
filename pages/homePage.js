4f4d7da7e5c2aa43de75c268e252bc15d7efbecd
Joe Young <musicjoeyoung@gmail.com>
2022-09-20 17:55:49 -0400

    initial commit

---
+++ pages/homePage.js
@@ -0,0 +1,15 @@
+import styles from "../styles/homePage.module.css";
+
+const homePage = () => {
+  return (
+    <>
+      <div className={styles.main}>
+        <h1>software engineer</h1>
+        Ideas: Have this page be one of those scrolling pages
+        <a href="/">Home</a>
+      </div>
+    </>
+  );
+};
+
+export default homePage;
---
+++ pages/homePage2.js
@@ -0,0 +1,15 @@
+import styles from "../styles/homePage2.module.css";
+
+const homePage2 = () => {
+  return (
+    <>
+      <div className={styles.main}>
+        <h1>musician</h1>
+        Ideas: Have this page be one of those scrolling pages
+        <a href="/">Home</a>
+      </div>
+    </>
+  );
+};
+
+export default homePage2;
