bf4dd14391aff8cfc0b4a88810f36ee1f6c67e8a
Joe Young <musicjoeyoung@gmail.com>
2022-12-09 11:29:47 -0500

    styles; arrow; 404

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import styles from "../styles/404.module.css";
+
+export default function Custom404() {
+  return (
+    <h1 className={styles.h1}>
+      Sorry! It looks like this page doesn't exist! Click{" "}
+      <a className={styles.a} href="/">
+        here
+      </a>{" "}
+      to go back home!
+    </h1>
+  );
+}
