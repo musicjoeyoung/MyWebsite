5e798a97bfb8662b360c78574aeb7614e743434b
Joe Young <musicjoeyoung@gmail.com>
2023-01-11 18:08:55 -0500

    styles; hamburger

--- pages/components/HamburgerMenu.js
+++ pages/components/HamburgerMenu.js
@@ -4,42 +4,58 @@ import Image from "next/image";
 
 const HamburgerMenu = () => {
   return (
-    <div className={styles.hamburgerMenu}>
-      <Image
-        src="/images/dropdown.png"
-        className={styles.dropdownbtn}
-        alt=""
-        width={35}
-        height={35}
-      />
-      <div className={styles.dropdownContent}>
-        <li className={styles.li}>
-          <Link href="/homePage" className={styles.glowOnHover}>
-            Home
-          </Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="#bio">About</Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="#projects">Projects</Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="#experience">Experience</Link>
-        </li>
-        <li className={styles.li}>
-          <Link
-            href="/images/JosephYoung_Resume_FrontEndDeveloper.pdf"
-            target="_blank"
-          >
-            Resume
-          </Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="#contact">Contact</Link>
-        </li>
+    <>
+      {" "}
+      <div className={styles.logo}>
+        <Link href="/">
+          <Image
+            src="/images/favicon.png"
+            alt="JY logo"
+            width={50}
+            height={50}
+          />
+        </Link>
       </div>
-    </div>
+      <div className={styles.body}>
+        <nav role="navigation">
+          <div className={styles.menuToggle}>
+            <input type="checkbox" />
+
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <ul className={styles.menu}>
+              <li className={styles.li}>
+                <Link href="/homePage" className={styles.glowOnHover}>
+                  Home
+                </Link>
+              </li>
+              <li className={styles.li}>
+                <Link href="#bio">About</Link>
+              </li>
+              <li className={styles.li}>
+                <Link href="#projects">Projects</Link>
+              </li>
+              <li className={styles.li}>
+                <Link href="#experience">Experience</Link>
+              </li>
+              <li className={styles.li}>
+                <Link
+                  href="/images/JosephYoung_Resume_FrontEndDeveloper.pdf"
+                  target="_blank"
+                >
+                  Resume
+                </Link>
+              </li>
+              <li className={styles.li}>
+                <Link href="#contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
