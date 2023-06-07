879c7757c11ad6dad7914db3e65bcd1c196ad159
Joe Young <musicjoeyoung@gmail.com>
2023-06-07 15:11:00 -0400

    unfinished branch work

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,16 @@
-import styles from "../styles/404.module.css";
+import styles from "../styles/404.module.scss";
 import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <>
-      <h1 className={styles.h1}>
+    <div className={styles.container}>
+      <h1 className={styles.container__h1}>
         Sorry! It looks like this page does not exist! Click{" "}
-        <Link className={styles.a} href="/">
+        <Link className={styles.container__a} href="/">
           here
         </Link>{" "}
         to go back home!
       </h1>
-    </>
+    </div>
   );
 }
